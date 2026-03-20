b6c2aef6a1f58e9c5e297a0dd75339d60c237097
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-03-20 13:45:55 -0300

    conta

--- atividade 4/conta.java
+++ atividade 4/conta.java
@@ -9,3 +9,27 @@ public class Conta {
         this.saldo = saldo;
         this.limite = limite;
     }
+
+     public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    
+    public void sacar(double valor) {
+        if (saldo + limite >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+   
+    public boolean usandoChequeEspecial() {
+        return saldo < 0;
+    }
+
+    
+    public double getSaldo() {
+        return saldo;
+    }
+}
