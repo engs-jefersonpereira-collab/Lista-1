fb7da1e0e4c44d244a09801ef06c8f4ea350053d
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-03-20 14:05:29 -0300

    musica

---
+++ atividade 6/musica.java
@@ -0,0 +1,13 @@
+package atividade6;
+
+public class Musica {
+     private String titulo;
+    private String artista;
+    private int duracao; 
+
+    
+    public Musica(String titulo, String artista, int duracao) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracao = duracao;
+    }
