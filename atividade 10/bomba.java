5c53f7cf192742ecdcfa7008e33ae51d1fe6f893
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-03-20 15:17:00 -0300

    bomba

---
+++ atividade 10/bomba.java
@@ -0,0 +1,12 @@
+package atividade10;
+
+public class BombaCombustivel {
+     private String tipoCombustivel;
+    private double valorLitro;
+    private double quantidadeCombustivel;
+
+    public BombaCombustivel(String tipoCombustivel, double valorLitro, double quantidadeCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+        this.valorLitro = valorLitro;
+        this.quantidadeCombustivel = quantidadeCombustivel;
+    }
