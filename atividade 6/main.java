bb597284d674e2c7551bb8c83035efe879b744dd
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-03-20 14:03:29 -0300

    main da musica

---
+++ atividade 6/main.java
@@ -0,0 +1,10 @@
+package atividade6;
+
+public class main {
+      public static void main(String[] args) {
+
+        Musica m1 = new Musica("Numb", "Linkin Park", 230);
+
+        System.out.println("Duração: " + m1.getDuracaoFormatada());
+    }
+}
